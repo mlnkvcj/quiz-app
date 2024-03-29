5610d66f58e06b1e0d327f21b576f9378a2488e3
Jovan Milinković <milinkovicjovan05@gmail.com>
2024-03-29 16:20:51 +0100

    početak

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -14,22 +14,6 @@
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
 					href="https://github.com/skeletonlabs/skeleton"
 					target="_blank"
 					rel="noreferrer"
